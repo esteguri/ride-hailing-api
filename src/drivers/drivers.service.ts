a40c6e6b9dcb8f56bcb4e66ca0751a8156f19ed2
Esteban Gutierrez <gutierrezesteban53@gmail.com>
2023-05-27 16:35:47 -0500

    ADD: drivers service created

--- src/drivers/drivers.service.ts
+++ src/drivers/drivers.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Driver } from './entities/driver.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class DriversService {}
+export class DriversService {
+  constructor(
+    @InjectRepository(Driver)
+    private readonly driversRepository: Repository<Driver>,
+  ) {}
+
+  public async findAll() {
+    return await this.driversRepository.find();
+  }
+
+  public async createAll(drivers: Driver[]) {
+    return await this.driversRepository.save(drivers);
+  }
+}
