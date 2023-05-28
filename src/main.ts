14623aa26787d1cda4496e7a9d76b2a7bae9aafd
Esteban Gutierrez <gutierrezesteban53@gmail.com>
2023-05-28 16:16:17 -0500

    ADD: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { JwtService } from '@nestjs/jwt';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Ride-Hailing Api')
+    .setDescription(
+      'Endpoint of the ride-hailing API. Author: @esteguri - Esteban Gutierrez',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
 
   const jwtService = new JwtService();
   const reflector = app.get('Reflector');
